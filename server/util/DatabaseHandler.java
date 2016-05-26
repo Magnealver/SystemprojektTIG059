8e20659efe81b96f4a257673a33e410e6a4b63b6
Magnealver <magne.alver@gmail.com>
2016-05-26 12:35:45 +0200

    Add files via upload

---
+++ server/util/DatabaseHandler.java
@@ -0,0 +1,42 @@
+package server.util;
+
+import java.sql.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+public class DatabaseHandler {
+	public Connection c;
+	Statement s;
+	PreparedStatement sm; 
+	ResultSet rs;
+
+	public DatabaseHandler(){
+		c = createConnection();
+	}
+
+	public Connection createConnection(){
+			Connection localConn = null;
+			try {
+				Class.forName("org.sqlite.JDBC");
+			} catch(ClassNotFoundException cnfe){
+				System.out.println("No driver found");
+			} 
+			System.out.println("Driver found!");
+			
+			try{
+				localConn = DriverManager.getConnection("jdbc:sqlite:server/util/databasen.db");
+			} catch (SQLException se){
+				System.out.println("Couldn't connect to the database");
+			}
+			
+			if(localConn !=null)
+				System.out.println("Connected to the database");
+			else
+				System.out.println("Couldn't connect to the database");
+			return localConn;
+		}
+	
+
+	
+}
